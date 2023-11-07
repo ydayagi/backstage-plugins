fc0a3184fcb7224f44576e37a96557b85e3a6640
Marek Libra <marek.libra@gmail.com>
2023-11-07 16:04:19 +0100

    Polling in NotificationsSidebarItem can be configured

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -80,7 +80,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         </SidebarScrollWrapper>
       </SidebarGroup>
       <SidebarDivider />
-      <NotificationsSidebarItem />
+      <NotificationsSidebarItem pollingInterval={5000} />
       <SidebarSpace />
       <SidebarDivider />
       <SidebarGroup
